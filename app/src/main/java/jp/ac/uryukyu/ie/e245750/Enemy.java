0e061841923fb6a02e2a36fee633faf79fbdcf49
naoya-ai <konakurisatsuma23@icloud.com>
2024-11-12 23:47:50 +0900

    step3

--- app/src/main/java/jp/ac/uryukyu/ie/e245750/Enemy.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e245750/Enemy.java
@@ -9,10 +9,10 @@ package jp.ac.uryukyu.ie.e245750;
  * Created by tnal on 2016/11/13.
  */
 public class Enemy {
-    public String name;
-    public int hitPoint;
-    public int attack;
-    public boolean dead;
+    private String name;
+    private int hitPoint;
+    private int attack;
+    private boolean dead;
 
     /**
      * コンストラクタ。名前、最大HP、攻撃力を指定する。
@@ -20,6 +20,7 @@ public class Enemy {
      * @param maximumHP モンスターのHP
      * @param attack モンスターの攻撃力
      */
+    
     public Enemy (String name, int maximumHP, int attack) {
         this.name = name;
         hitPoint = maximumHP;
@@ -38,7 +39,7 @@ public class Enemy {
             return;
         }
         int damage = (int)(Math.random() * attack);
-        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.name, damage);
+        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
         hero.wounded(damage);
     }
 
@@ -53,6 +54,31 @@ public class Enemy {
             dead = true;
             System.out.printf("モンスター%sは倒れた。\n", name);
         }
+    }
+    public String getName(){
+        return name;
+    }
+    public int getHitPoint(){
+        return hitPoint;
+    }
+    public int getAttack(){
+        return attack;
+    }
+    public boolean getDead(){
+        return dead;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setHitPoint(int hitPoint){
+        this.hitPoint = hitPoint;
+    }
+    public void setAttack(int attack){
+        this.attack = attack;
+    }
+    public void setDead(boolean dead){
+        this.dead = dead;
     }
 }
 
